fac53b953221d7cfe38499426db7b0e5415eb4cb
Ryan Stevens <restevens52@gmail.com>
2022-05-24 14:50:34 -0700

    checking to see if I fixed it

---
+++ FFT.m
@@ -0,0 +1,14 @@
+function [Ftransform] = FFT(signal, length)
+%FFT Implement my first FFT function
+%   Uses FFT to find the f transform of a discrete data array
+%   signal: an array of real values
+%   Ftransform: the fourier transform of signal containing frequencies
+%   of max frequency length/2
+
+dfb[m
+
+
+
+
+end
+
--- README.md
+++ README.md
@@ -3,6 +3,6 @@ restevens52@gmail.com
 
 
 Here I will be implementing some fourier transform methods as well as some 
-signal generating functions in MATLAB. 
+signal generating functions in MATLAB. something wicked this way comes
 
 
